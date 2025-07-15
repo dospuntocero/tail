216f0076069545b509ba206057361215521eadec
francisco arenas <fa@dospuntocero.cl>
2025-07-14 21:28:18 -0400

    super testimonios

--- src/gastronomia/gastronomia.jsx
+++ src/gastronomia/gastronomia.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import '../index.css';
+import '../output.css';
 import Header from '../secciones/Header';
 import Footer from '../secciones/Footer';
 import Menu from '../componentes/Menu';
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
 
   theme: {
-    extend: {
-      colors: {
-        "primary": "#c48f3a",
-        "secondary": "#000000",
-        "terciary": "#f2f2f2",
-        "accent": "#c5a572",
-
-      },
-    },
+  
   },
   plugins: [],
 }
